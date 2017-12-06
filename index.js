bb49962571f8ab4bee76fbf1a902906891a21c14
khrystynac9 <khrystynkakitty@gmail.com>
2017-12-06 19:35:33 +0200

    correction

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function moneyExchange() {
     dollar = 27.16;
     ration = euro / dollar;
     ration2 = ration.toFixed(2);
-    if (euroEmount && dollarEmount) {
+    if (euroEmount < 0 || dollarEmount < 0) {
+        alert("Please enter the cerrect amount of euros or dollars");
+    } else if(euroEmount || dollarEmount) {
         euroSum = euroEmount * euro;
         dollarSum = dollarEmount * dollar;
         alert(euroEmount + " euros are equal to " + euroSum.toFixed(2) + " grns, " + dollarEmount + " dollars are equal to " + dollarSum.toFixed(2) + " grns, one euro is equal to " + ration2 + " dollars.")
