79c7fc458ab7d01180f15c104d4b39b5d222d08b
khrystynac9 <khrystynkakitty@gmail.com>
2017-12-07 12:41:18 +0200

    made some changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var euroEmount = +prompt("Please enter the amount of euros", 100);
-var dollarEmount = +prompt("Please enter the amount of dollars", 100);
 function moneyExchange() {
     var euroSum, dollarSum, euro, dollar, ratio, ration2;
+    do {
+        var euroEmount = +prompt("Please enter the amount of euros", 100);
+    }
+    while (euroEmount < 0 || isNaN(euroEmount));
+    do {
+        var dollarEmount = +prompt("Please enter the amount of dollars", 100);
+    }
+    while (dollarEmount < 0 || isNaN(dollarEmount));
     euro = 32.18;
     dollar = 27.16;
     ration = euro / dollar;
     ration2 = ration.toFixed(2);
-    if (euroEmount < 0 || dollarEmount < 0) {
-        alert("Please enter the cerrect amount of euros or dollars");
-    } else if(euroEmount || dollarEmount) {
-        euroSum = euroEmount * euro;
-        dollarSum = dollarEmount * dollar;
-        alert(euroEmount + " euros are equal to " + euroSum.toFixed(2) + " grns, " + dollarEmount + " dollars are equal to " + dollarSum.toFixed(2) + " grns, one euro is equal to " + ration2 + " dollars.")
-    } else {
-        alert("Please enter the cerrect amount of euro or dollars")
-    }
+    euroSum = euroEmount * euro;
+    dollarSum = dollarEmount * dollar;
+    alert(euroEmount + " euros are equal to " + euroSum.toFixed(2) + " grns, " + dollarEmount + " dollars are equal to " + dollarSum.toFixed(2) + " grns, one euro is equal to " + ration2 + " dollars.");
 }
 moneyExchange();
